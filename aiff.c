062b64c93e1ec5ede8b0d1fde91d9583e34d9adf
Datong Sun <dsun@iastate.edu>
2013-10-07 00:38:26 -0500

    New function write_aiff_header() added.

--- aiff.c
+++ aiff.c
@@ -81,6 +81,62 @@ struct soundfile aiff_fileinfo(FILE *file)
     return info;
 }
 
+void write_aiff_header(FILE *ofile, const struct soundfile *info)
+{
+    unsigned int temp_32;
+    unsigned short temp_16;
+
+    fputs("FORM", ofile);
+    
+    temp_32 = 4 + 4 + 4 + 18 + 4 + 4 + 4 + 4
+              + (info->bit_depth / 8) * info->channels * info->sample_num;
+    temp_32 = htonl(temp_32); /* File SIze */
+    fwrite(&temp_32, 4, 1, ofile);
+
+    fputs("AIFFCOMM", ofile);
+
+    temp_32 = htonl(18); /* COMM Chunk size */
+    fwrite(&temp_32, 4, 1, ofile);
+
+    temp_16 = htons(info->channels); /* NumChannels  */
+    fwrite(&temp_16, 2, 1, ofile);
+
+    temp_32 = htonl(info->sample_num); /* NumSampleFrames */
+    fwrite(&temp_32, 4, 1, ofile);
+
+    temp_16 = htons(info->bit_depth); /* SampleSize */
+    fwrite(&temp_16, 2, 1, ofile);
+
+    unsigned long fraction;
+    fraction = info->sample_rate;
+    int i;
+
+    for (i=63; i>=0; i--)
+    {
+        if (fraction & (0x01UL << i)) /* Found the integer */
+            break; 
+    }
+
+    fraction <<= 63 - i;
+    fraction = byte_swap_64(fraction);
+
+    temp_16 = htons(i + 16383);
+
+    fwrite(&temp_16, 2, 1, ofile);
+    fwrite(&fraction, 8, 1, ofile);
+
+    fputs("SSND", ofile);
+
+    temp_32 = (info->bit_depth / 8) * info->channels * info-> sample_num + 8;
+    temp_32 = htonl(temp_32);
+    /* Chunk size */
+    fwrite(&temp_32, 4, 1, ofile);
+
+    temp_32 = 0;
+    fwrite(&temp_32, 4, 1, ofile); /* Offset */
+    fwrite(&temp_32, 4, 1, ofile); /* BlockSize */
+}
+
 int aiff_enumerate(FILE *file, const struct soundfile *info, sample_cb cb, void *data)
 {
     char buffer[128];
--- aiff.h
+++ aiff.h
@@ -2,5 +2,6 @@
 
 int is_aiff_file(FILE *file);
 struct soundfile aiff_fileinfo(FILE *file);
+void write_aiff_header(FILE *ofile, const struct soundfile *info);
 int aiff_enumerate(FILE *file, const struct soundfile *info, sample_cb cb, void *data);
 
