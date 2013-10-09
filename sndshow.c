e7328f32064f50739cd7ad5244fa568cbf1ac395
Datong Sun <dsun@iastate.edu>
2013-10-08 23:48:26 -0500

    Fixed sndshow factor negative number

--- sndshow.c
+++ sndshow.c
@@ -173,6 +173,9 @@ int main(int argc, char *argv[])
 
             case 'z':
                 z = atoi(optarg);
+                
+                if (z < 1)
+                    FATAL("Zoom factor must greater than 0");
                 break;
 
             case '?':
